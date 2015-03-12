fd71694397d812cab686aa3e9fdb7b654048209d
George <guopengsheng@gmail.com>
2015-03-12 22:13:14 +0800

    上传所有数据到server

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,8 +11,6 @@ angular.module('starter.controllers', [])
       if(data["code"] == 1) {
         window.location = "#/tab/status";
       } else {
-        //alert("username or password is not correct!");
-        // Triggered on a button click, or some other target
         $scope.showAlert = function() {
           var alertPopup = $ionicPopup.alert({
             title: 'Invalid Password',
@@ -34,51 +32,54 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('appotCategoryCtrl', function($scope) {
-  $scope.cats = [{id:"dental",name:"Dental"}, {id:"ent",name:"ENT"}, {id:"womenhealth", name:"Women's Health"}]
-  $scope.click = function(cat) {
-    window.localStorage.category = cat;
-    window.location = "#/tab/status/category/lad";
-  }
+.controller('appotCategoryCtrl', function($scope, DisplayCat) {
+  DisplayCat.catRequest(function(data){
+    $scope.cats = angular.copy(data);
+
+    $scope.click = function(cat) {
+      window.localStorage.category = cat;
+      window.location = "#/tab/status/category/lad";
+    }
+  });
 })
 
-.controller('DateLocationCtrl', function($scope) {
-  $scope.locations = [{id:"none",name: " "}, {id:"jurong_point", name: "Jurong Point"}, {id:"bishan", name: "Bishan"}, 
-  {id:"changi_airport", name: "Changi Airport"}]
-  $scope.dates = [{id:"none",name: " "}, {id:"2015-03-08", name: "March 8th"}, {id:"2015-03-09", name: "March 9th"}, 
-  {id:"2015-03-10", name: "March 10th"}]
-  
+.controller('DateLocationCtrl', function($scope, DisplayDateLoc) {
+  DisplayDateLoc.datelocRequest(function(data){
+    $scope.datelocation = angular.copy(data);
+    $scope.locations = $scope.datelocation.location;
+    $scope.dates = $scope.datelocation.date;
+
+    
+  });
   $scope.click = function() {
-    window.location = "#/tab/status/category/lad/doctors";
-    window.localStorage.location = document.getElementById("location").value;
-    window.localStorage.date = document.getElementById("date").value;
+      window.location = "#/tab/status/category/lad/doctors";
+      window.localStorage.location = document.getElementById("location").value;
+      window.localStorage.date = document.getElementById("date").value;
   }
 })
 
-.controller('DoctorsCtrl', function($scope) {
-  $scope.doctors = [{img: "img/doc1.png", name: "Marty Mcfly", profile: "General Practitioner, Women Health", 
-  times: ["08:30", "09:30", "10:30", "11:30", "12:30", "13:30"]}, 
-  {img: "img/doc2.png", name: "Mary Fung", profile: "General Practitioner, Dental", 
-  times: ["09:30", "10:30", "11:30", "12:30", "13:30"]}, 
-  {img: "img/doc3.png", name: "Tom Hagons", profile: "General Practitioner", 
-  times: ["08:30", "11:30", "12:30", "13:30"]}]
+.controller('DoctorsCtrl', function($scope, DisplayDoc) {
+  DisplayDoc.docRequest(function(data){
+    $scope.doctors = angular.copy(data);
+  });
 
   $scope.goto = function(doctor) {
     window.localStorage.doctor = JSON.stringify(doctor)
     window.location = "#/tab/status/category/lad/doctors/specific"
+    console.log(doctor);
   }
 
   $scope.date = "(" + window.localStorage.date + ")"
 })
 
 .controller('DoctorsSpecificCtrl', function($scope) {
-  doctor = JSON.parse(window.localStorage.doctor)
-  $scope.name = doctor.name
-  $scope.img = doctor.img
-  $scope.profile = doctor.profile
-  $scope.date = window.localStorage.date
+  doctor = JSON.parse(window.localStorage.doctor);
+  $scope.name = doctor.name;
+  $scope.img = doctor.img;
+  $scope.profile = doctor.profile;
+  $scope.detail = doctor.detail;
+  $scope.date = window.localStorage.date;
 
-  $scope.detail = "Dr Marty Mcfly obtained her Master’s degree in Internal Medicine from the National University of Singapore and membership of the Royal College of Physicians (UK) in 2002. She received her specialist accreditation in Respiratory Medicine in Singapore in 2005 and was awarded the European Diploma in Intensive Care by the European Society of Intensive Care Medicine in 2006. She completed further training in Advanced Pulmonology and Multi-Disciplinary Intensive Care Medicine under a Singapore Ministry of Health scholarship at the Cleveland Clinic in Ohio, USA in 2009 and was elected a Fellow Physician of the American College of Chest Physicians."
   times = doctor.times;
   $scope.timerows = Array(Math.floor((times.length + 2) / 3));
   for(var i=0; i<$scope.timerows.length; i++) {
--- www/js/services.js
+++ www/js/services.js
@@ -4,7 +4,10 @@ angular.module('starter.services', [])
   var baseUrl = "http://128.199.219.37/"
 
   return {
-    login: baseUrl + "login.php"
+    login: baseUrl + "login.php",
+    category: baseUrl + "category_info.php",
+    dalocation: baseUrl + "datelocation_info.php",
+    doctors: baseUrl + "doctors_info.php"
   }
 })
 
@@ -20,4 +23,46 @@ angular.module('starter.services', [])
   return {
     login: login
   }
-});
+})
+
+.factory('DisplayCat', function($http, ServerURL) {
+  var catRequest = function(callback){
+    $http
+    .post(ServerURL.category, {"requestForCategory": "true"})
+    .success(function(data){
+      callback(data);
+    });
+  } 
+
+  return {
+    catRequest: catRequest
+  }
+})
+
+.factory('DisplayDateLoc', function($http, ServerURL) {
+  var datelocRequest = function(callback){
+    $http
+    .post(ServerURL.dalocation, {"requestForLad": "true"})
+    .success(function(data){
+      callback(data);
+    });
+  } 
+
+  return {
+    datelocRequest: datelocRequest
+  }
+})
+
+.factory('DisplayDoc', function($http, ServerURL) {
+  var docRequest = function(callback){
+    $http
+    .post(ServerURL.doctors, {"requestForDoc": "true"})
+    .success(function(data){
+      callback(data);
+    });
+  } 
+
+  return {
+    docRequest: docRequest
+  }
+})
